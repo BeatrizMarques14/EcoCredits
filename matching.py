f3fdb63ac1866e41f58abd6a8b472396a7bdc82c
BeatrizMarques14 <marques.beatriz14@ua.pt>
2024-04-20 12:10:58 +0100

    alteraçoes ao matching

--- matching.py
+++ matching.py
@@ -3,42 +3,71 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 
 # read two input images as grayscale
-img1 = cv.imread('azul.png',cv.IMREAD_GRAYSCALE) # queryImage
+img_azul = cv.imread('azul.png',cv.IMREAD_GRAYSCALE) # queryImage
+img_verde = cv.imread('verde.png',cv.IMREAD_GRAYSCALE)
+img_amarelo = cv.imread('amarelo.png',cv.IMREAD_GRAYSCALE)
 img2 = cv.imread('rotulo1.png',cv.IMREAD_GRAYSCALE) # trainImage
 # Initiate SIFT detector
 sift = cv.SIFT_create()
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
-# BFMatcher with default params
+# # BFMatcher with default params
 bf = cv.BFMatcher()
-matches = bf.knnMatch(des1,des2,k=2)
-# Apply ratio test
-good = []
-for m,n in matches:
-    if m.distance < 0.95*n.distance:
-        good.append([m])
-print(len(good))
-# cv.drawMatchesKnn expects list of lists as matches.
-# img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-# plt.imshow(img3),plt.show() 
+
 
 #im2, contours = cv.findContours(img2, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-edged = cv.Canny(img2, 150, 300) 
+edged = cv.Canny(img2, 50, 400) 
 cv.waitKey(0) 
   
 # Finding Contours 
 # Use a copy of the image e.g. edged.copy() 
 # since findContours alters the image 
 contours, hierarchy = cv.findContours(edged, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE) 
-largest_contour = max(contours, key=cv.contourArea)
-print(len(contours))
+#contours = sorted(contours, key=cv.contourArea, reverse=True)
+contours = sorted(contours, key=lambda x: cv.boundingRect(x)[2] * cv.boundingRect(x)[3], reverse=True)
+
+
+x_max, y_max, w_max, h_max = cv.boundingRect(contours[0])
 
   
 cv.imshow('Canny Edges After Contouring', edged) 
 cv.waitKey(0) 
 
-cv.drawContours(img2, [largest_contour], -1, (0, 255, 0), 2)
+cv.drawContours(img2, [contours[0]], -1, (0, 255, 0), 2)
+
+good_amarelo=[]
+good_azul=[]
+good_verde=[]
+for contour in contours:
+     x, y, w, h = cv.boundingRect(contour)
+
+     if (h> h_max-h_max/2) or (w > w_max-w_max/2):  
+        box = img2[y:y+h, x:x+w]
+        kpa1, desa1 = sift.detectAndCompute(img_azul,None)
+        kpv1, desv1 = sift.detectAndCompute(img_verde,None)
+        kpam1, desam1 = sift.detectAndCompute(img_amarelo,None)
+        kp2, des2 = sift.detectAndCompute(img2,None)
+        
+        matches_azul = bf.knnMatch(desa1,des2,k=2)
+        # Apply ratio test
+        for m,n in matches_azul:
+            if m.distance < 0.95*n.distance:
+                good_azul.append([m])
+        
+        matches_verde = bf.knnMatch(desv1,des2,k=2)
+        # Apply ratio test
+        for m,n in matches_verde:
+            if m.distance < 0.95*n.distance:
+                good_verde.append([m])
+        
+        matches_amarelo = bf.knnMatch(desam1,des2,k=2)
+        # Apply ratio test
+        for m,n in matches_amarelo:
+            if m.distance < 0.95*n.distance:
+                good_amarelo.append([m])
+            
+
+print('Amarelo: ' + str(len(good_amarelo)))
+print('Azul: ' + str(len(good_azul)))
+print('Verde: ' + str(len(good_verde)))    
 
   
 cv.imshow('Contours', img2) 
